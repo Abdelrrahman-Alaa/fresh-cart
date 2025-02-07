7c8f9318e6e5187df90651c7ec6ca954d2e9314c
Abdelrahman Alaa <Abdelrrahman.Alaa@gmail.com>
2025-02-07 13:48:10 +0200

    change icon and add descreption

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -143,7 +143,7 @@ export default function Navbar() {
               </button>
             </div>
             <div className="mt-6 flow-root">
-              <div className="-my-6 divide-y divide-gray-500/10">
+              <div className="-my-6 divide-y divide-gray-500/10 text-center">
                 {userToken && (
                   <div className="space-y-2 py-6 capitalize">
                     <NavLink
